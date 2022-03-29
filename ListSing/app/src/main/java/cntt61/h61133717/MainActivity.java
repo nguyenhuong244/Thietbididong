cb4488a73adfdf18e35233b7fd0514d41058cb53
your name <huong.nmd.61kt@ntu.edu.vn>
2022-03-29 18:06:45 +0700

    song

--- ListSing/app/src/main/java/cntt61/h61133717/MainActivity.java
+++ ListSing/app/src/main/java/cntt61/h61133717/MainActivity.java
@@ -11,9 +11,9 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
-    String[] baihat={"Sugar","Memories","Payphone"};
-    String[] casi={"Maroon5","Maroon5","Maroon5"};
-    Integer[] img={R.drawable.dl1,R.drawable.dl2,R.drawable.dl3};
+    String baihat[]={"Sugar","Memories","Payphone"};
+    String casi[]={"Maroon5","Maroon5","Maroon5"};
+    Integer img[]={R.drawable.dl1,R.drawable.dl2,R.drawable.dl3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,36 +21,13 @@ public class MainActivity extends AppCompatActivity {
         Data adapter=new Data(this, baihat, casi,img);
         listView=(ListView)findViewById(R.id.lv);
         listView.setAdapter(adapter);
-
+        TextView textView=(TextView) findViewById(R.id.txt);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,int position, long id) {
-                // TODO Auto-generated method stub
-                if(position == 0) {
-                    //code specific to first list item
-                    Toast.makeText(getApplicationContext(),"Place Your First Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 1) {
-                    //code specific to 2nd list item
-                    Toast.makeText(getApplicationContext(),"Place Your Second Option Code",Toast.LENGTH_SHORT).show();
-                }
-
-                else if(position == 2) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Third Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 3) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Forth Option Code",Toast.LENGTH_SHORT).show();
-                }
-                else if(position == 4) {
-
-                    Toast.makeText(getApplicationContext(),"Place Your Fifth Option Code",Toast.LENGTH_SHORT).show();
-                }
-
+                textView.setText("Vị trí: "+position+" ; Bài hát: "+baihat[position]);
             }
         });
     }
