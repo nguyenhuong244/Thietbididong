24506f52cf52ff46d83bc24ca98c5774db2964ad
your name <huong.nmd.61kt@ntu.edu.vn>
2022-04-05 15:19:02 +0700

    upload

--- BaiKT2/app/src/main/java/NguyenMaiDiemHuong/BaiKT2/MainActivity.java
+++ BaiKT2/app/src/main/java/NguyenMaiDiemHuong/BaiKT2/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     RadioButton r_13, r_15, r_18;
     Button tinh;
+    EditText bp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bp=(EditText) findViewById(R.id.edtN);
         r_13=(RadioButton) findViewById(R.id.radio_13);
         r_15=(RadioButton) findViewById(R.id.radio_15);
         r_18=(RadioButton) findViewById(R.id.radio_18);
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         tinh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                float nhap=Float.parseFloat(r_13.getText().toString());
+                float nhap=Float.parseFloat(bp.getText().toString());
                 if(r_13.isChecked()){
                     float tt= (float) (nhap*0.13);
                     Toast.makeText(MainActivity.this, "Your tip will be $"+tt, Toast.LENGTH_SHORT).show();
