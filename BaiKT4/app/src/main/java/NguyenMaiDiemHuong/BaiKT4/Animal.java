4b3b9db2c6e12864a32d8b067bb888d163c6cea1
your name <huong.nmd.61kt@ntu.edu.vn>
2022-04-06 13:13:08 +0700

    upload

---
+++ BaiKT4/app/src/main/java/NguyenMaiDiemHuong/BaiKT4/Animal.java
@@ -0,0 +1,37 @@
+package NguyenMaiDiemHuong.BaiKT4;
+
+public class Animal {
+    private String Ten;
+    private String MoTa;
+    private int Hinh;
+
+    public Animal(String ten, String moTa, int hinh) {
+        Ten = ten;
+        MoTa = moTa;
+        Hinh = hinh;
+    }
+
+    public String getTen() {
+        return Ten;
+    }
+
+    public void setTen(String ten) {
+        Ten = ten;
+    }
+
+    public String getMoTa() {
+        return MoTa;
+    }
+
+    public void setMoTa(String moTa) {
+        MoTa = moTa;
+    }
+
+    public int getHinh() {
+        return Hinh;
+    }
+
+    public void setHinh(int hinh) {
+        Hinh = hinh;
+    }
+}
