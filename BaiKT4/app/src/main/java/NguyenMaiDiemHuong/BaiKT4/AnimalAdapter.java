e2e61d22b34113cdf0d91e96d8a9f7a9c6720272
your name <huong.nmd.61kt@ntu.edu.vn>
2022-04-06 13:22:59 +0700

    upload

--- BaiKT4/app/src/main/java/NguyenMaiDiemHuong/BaiKT4/AnimalAdapter.java
+++ BaiKT4/app/src/main/java/NguyenMaiDiemHuong/BaiKT4/AnimalAdapter.java
@@ -1,13 +1,30 @@
 package NguyenMaiDiemHuong.BaiKT4;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class AnimalAdapter extends BaseAdapter {
+
+    private Context context;
+    private int layout;
+    private List<Animal> animalList;
+
+    public AnimalAdapter(Context context, int layout, List<Animal> animalList) {
+        this.context = context;
+        this.layout = layout;
+        this.animalList = animalList;
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return animalList.size();
     }
 
     @Override
@@ -22,6 +39,14 @@ public class AnimalAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = inflater.inflate(layout,null);
+
+        TextView tvten = (TextView) view.findViewById(R.id.tvTen);
+        TextView tvmota = (TextView) view.findViewById(R.id.tvMoTa);
+        ImageView imageView = (ImageView) view.findViewById(R.id.img);
+
+        
         return null;
     }
 }
