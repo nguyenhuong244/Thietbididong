ae39bf4d8129467d80564a9f9de85b2c5d88cb23
your name <huong.nmd.61kt@ntu.edu.vn>
2022-04-12 14:09:30 +0700

    upload

--- VD/app/src/main/java/vn/ntu/vd/MainActivity.java
+++ VD/app/src/main/java/vn/ntu/vd/MainActivity.java
@@ -4,14 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    static final String[] NATIONS = new String[]{"Australia","England","VietNam","Albania","USA","Algeria"};
+    AutoCompleteTextView autoTV;
+    ArrayList<String> nationsList;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String> adapter;
-        
+
+        nationsList = new ArrayList<>();
+        nationsList.add("Australia");
+        nationsList.add("England");
+        nationsList.add("VietNam");
+        nationsList.add("Albania");
+        nationsList.add("USA");
+        nationsList.add("Algeria");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,nationsList);
+        autoTV = (AutoCompleteTextView) findViewById(R.id.attv);
+        autoTV.setAdapter(adapter);
     }
 }
